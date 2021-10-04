662e0de5d2b59b22c42a93ca23e3f746a7e999c2
uladzidu <vdukhovnik1@gmail.com>
2021-10-04 21:35:44 +0200

    commit grasshopper-summation

--- Grasshopper - Summation.js
+++ Grasshopper - Summation.js
@@ -1,3 +1,8 @@
+/*
+Write a program that finds the summation of every number from 1 to num. 
+The number will always be a positive integer greater than 0.
+*/
+
 var summation = function (num) {
   let result = 0;
   for (var i = 1; i <= num; i++) {
