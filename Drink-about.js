37094f04c6f3acbec459186d4b3203271b5e18f8
uladzidu <vdukhovnik1@gmail.com>
2021-10-10 20:10:31 +0200

    2 account (vlad_prog2) Compleated tasks. 10.10.2021

---
+++ Drink-about.js
@@ -0,0 +1,11 @@
+function peopleWithAgeDrink(old) {
+  if (old < 14) {
+    return "drink toddy";
+  } if (old < 18) {
+    return "drink coke";
+  } if (old < 21) {
+    return "drink beer";
+  } else {
+  return "drink whisky";
+    }
+};
---
+++ Function-2-squaring-an-argument.js
@@ -0,0 +1,4 @@
+// Write the "square"-function here
+function square(argument) {
+  return Math.pow(argument,2);
+}
